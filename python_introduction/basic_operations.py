e0f6f11b36f9e9ac178ffdf5db4c124a006d2558
0xelochukwu <charleselochukwumagbo@gmail.com>
2025-09-11 02:38:32 +0100

    added basic_operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,14 @@
+#picking numbers
+number1 = 50
+number2 = 100
+
+#calculating the sum of the two variables
+calcSum = number2 + number1
+print("Addition of " + str(number2) + " and " + str(number1) + " is " + str(calcSum))
+
+#calculating the differences between the two variables
+calcMinus = number2 - number1
+print("Subtraction of " + str(number2) + " and " + str(number1) + " is " + str(calcMinus))
+#multiplication the division of the two variables
+calcMultiple = number2 * number1
+print("Multiplication of " + str(number2) + " and " + str(number1) + " is " + str(calcMultiple))
