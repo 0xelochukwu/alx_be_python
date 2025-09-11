527b4438cfc2fb304b5c871534c9ffa3db7da67c
0xelochukwu <charleselochukwumagbo@gmail.com>
2025-09-11 02:43:36 +0100

    edited basic_operations.py

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,14 +1,15 @@
 #picking numbers
-number1 = 50
-number2 = 100
+number1 = 10
+number2 = 5
 
 #calculating the sum of the two variables
 calcSum = number2 + number1
 print("Addition of " + str(number2) + " and " + str(number1) + " is " + str(calcSum))
 
 #calculating the differences between the two variables
-calcMinus = number2 - number1
+calcMinus = number1 - number2
 print("Subtraction of " + str(number2) + " and " + str(number1) + " is " + str(calcMinus))
+
 #multiplication the division of the two variables
 calcMultiple = number2 * number1
 print("Multiplication of " + str(number2) + " and " + str(number1) + " is " + str(calcMultiple))
