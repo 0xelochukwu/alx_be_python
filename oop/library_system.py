b96c44acb32c6e6cce31b22260cd837e07303402
0xelochukwu <charleselochukwumagbo@gmail.com>
2025-10-12 21:23:13 +0100

    changes

--- oop/library_system.py
+++ oop/library_system.py
@@ -4,16 +4,17 @@ class Book:
         self.author = author
 
     def __str__(self):
-        return f"'{self.title}' by {self.author}"
+        return f"Book: {self.title} by {self.author}"
 
 
 class EBook(Book):
     def __init__(self, title: str, author: str, file_size: int):
         super().__init__(title, author)
-        self.file_size = file_size  # in MB
+        self.file_size = file_size  # file size in MB
 
     def __str__(self):
-        return f"EBook: {super().__str__()}, File Size: {self.file_size}MB"
+        file_size_kb = self.file_size * 1024
+        return f"EBook: {self.title} by {self.author}, File Size: {file_size_kb}KB"
 
 
 class PrintBook(Book):
@@ -22,7 +23,7 @@ class PrintBook(Book):
         self.page_count = page_count
 
     def __str__(self):
-        return f"PrintBook: {super().__str__()}, Pages: {self.page_count}"
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
 
 class Library:
